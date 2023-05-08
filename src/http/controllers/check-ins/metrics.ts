91228606a6f5b7d29cfa43f151b22156fadb98ad
Walison Fernandes <walisonvfernandes@hotmail.com>
2023-05-08 11:25:46 -0300

    fix: fixing importations in controllers

--- src/http/controllers/check-ins/metrics.ts
+++ src/http/controllers/check-ins/metrics.ts
@@ -1,6 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
-
+import { makeGetUserMetricsUseCase } from '../../../use-cases/factories/make-get-user-metrics-use-case'
 export async function metrics(request: FastifyRequest, reply: FastifyReply) {
   const getUserMetricsUseCase = makeGetUserMetricsUseCase()
 
